92244537014cbf8399ce82e8802a8b14a4181db5
MuTtonye91 <murielle.akoo@live.fr>
2019-01-14 12:36:39 +0100

    Ajout fichier calculator.rb

---
+++ lib/02_calculator.rb
@@ -0,0 +1,23 @@
+def add (x,y)
+	return x + y
+end
+
+def subtract (x,y)
+	return x - y
+end
+
+def sum (x=[])
+	return x.sum
+end
+
+def multiply (x, y)
+	return x * y
+end
+
+def power
+
+end
+
+def factorial
+
+end
